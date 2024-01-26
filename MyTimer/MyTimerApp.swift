a785f3fba03809c54f259322ce771a6399f4ce3e
gadgelogger <sarayou273142910@gmail.com>
2024-01-26 21:19:17 +0900

    Initial Commit

---
+++ MyTimer/MyTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTimerApp.swift
+//  MyTimer
+//
+//  Created by gadgelogger on 2024/01/26.
+//
+
+import SwiftUI
+
+@main
+struct MyTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
